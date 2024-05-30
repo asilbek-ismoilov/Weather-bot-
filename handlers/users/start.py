b46ed883a855edfc8c204d30be3dd93f6310d80e
asilbe-admin <asilbe.m00d@gmail.com>
2024-05-30 22:28:24 +0500

    Weather bot

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -2,6 +2,7 @@ from aiogram.types import Message
 from loader import dp,db
 from aiogram.filters import CommandStart
 from keyboard_buttons.weather_button import obh_button
+from keyboard_buttons.weather_inl_b import weather_inl_button
 
 @dp.message(CommandStart())
 async def start_command(message:Message):
@@ -9,6 +10,6 @@ async def start_command(message:Message):
     telegram_id = message.from_user.id
     try:
         db.add_user(full_name=full_name,telegram_id=telegram_id) #foydalanuvchi bazaga qo'shildi
-        await message.answer(text="Assalomu alaykum, botimizga hush kelibsiz", reply_markup=obh_button)
+        await message.answer(text="Assalomu alaykum, Ob-Havo botimizga hush kelibsiz", reply_markup=obh_button)
     except:
-        await message.answer(text="Assalomu alaykum", reply_markup=obh_button)
+        await message.answer(text="Assalomu alaykum", reply_markup=weather_inl_button)
